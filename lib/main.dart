886a9e31b747948fe0f18a6d4c7fc41ee4da712d
howkmii <abdelhakim@univ-tiaret.dz>
2021-10-13 19:36:27 +0100

    GetMaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 import 'ui/pages/notification_screen.dart';
 import 'ui/pages/home_page.dart';
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return GetMaterialApp(
       title: 'Mii Todo',
       debugShowCheckedModeBanner: false,
       home: HomePage(),
