356afe2007f36f67245003534431327ad03920d9
howkmii <abdelhakim@univ-tiaret.dz>
2021-10-16 12:44:42 +0100

    theme_services

--- lib/services/theme_services.dart
+++ lib/services/theme_services.dart
@@ -1,1 +1,14 @@
-class ThemeServices {}
+import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+
+class ThemeServices {
+  final GetStorage _box = GetStorage();
+  final _key = 'isDarkMode';
+  bool _loadThemeFromBox(){
+    return false;
+  }
+  ThemeMode get theme =>_loadThemeFromBox()? ThemeMode.dark:ThemeMode.light;
+  void switTheme(){
+
+  }
+}
