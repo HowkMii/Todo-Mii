0be95725f5908ed934c30c3cafd194a9c5224ebe
howkmii <abdelhakim@univ-tiaret.dz>
2021-10-16 13:51:58 +0100

    change theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
+import 'package:todomii/services/theme_services.dart';
 import 'package:todomii/ui/theme.dart';
 import 'ui/pages/notification_screen.dart';
 import 'ui/pages/home_page.dart';
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
     return  GetMaterialApp(
       theme: Themes.light,
       darkTheme: Themes.Dark,
-      themeMode: ThemeMode.light,
+      themeMode: ThemeServices().theme,
       title: 'Mii Todo',
       debugShowCheckedModeBanner: false,
       home:const HomePage(),
