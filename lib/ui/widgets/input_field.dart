a465b0448b05de31dd3107ba6ad80c46c3f568ab
howkmii <abdelhakim@univ-tiaret.dz>
2021-10-17 21:14:32 +0100

    input field

--- lib/ui/widgets/input_field.dart
+++ lib/ui/widgets/input_field.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 class InputField extends StatelessWidget {
-  const InputField({Key? key}) : super(key: key);
-
+  const InputField({Key? key, required this.label, required this.title, this.controller, this.widget}) : super(key: key);
+  final String label;
+  final String title;
+  final TextEditingController? controller;
+  final Widget? widget;
   @override
   Widget build(BuildContext context) {
     return Container();
