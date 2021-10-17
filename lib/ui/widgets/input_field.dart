6566a48d57777084b3a891f7323e4cc83a7c1638
howkmii <abdelhakim@univ-tiaret.dz>
2021-10-17 21:17:23 +0100

    input field

--- lib/ui/widgets/input_field.dart
+++ lib/ui/widgets/input_field.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../theme.dart';
+
 class InputField extends StatelessWidget {
   const InputField({Key? key, required this.label, required this.title, this.controller, this.widget}) : super(key: key);
   final String label;
@@ -8,6 +10,14 @@ class InputField extends StatelessWidget {
   final Widget? widget;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30,vertical:10 ),
+      margin: const EdgeInsets.symmetric(horizontal: 30),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        color:primaryClr,
+      )
+
+    );
   }
 }
