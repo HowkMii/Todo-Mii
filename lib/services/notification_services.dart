e7ab7345b0c339f7cc004c2c01d3f38c119363ff
howkmii <abdelhakim@univ-tiaret.dz>
2021-10-26 19:28:09 +0100

    local notification

--- lib/services/notification_services.dart
+++ lib/services/notification_services.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:get/get.dart';
 
 class NotifyHelper {
   static const AndroidInitializationSettings InitializationSettingsAndroid =
       AndroidInitializationSettings('app_icon');
   Future onDidReceiveLocalNotification(
-      int id, String body, String payload) async {}
+      int id, String body, String payload) async {
+    Get.dialog(Text(body));
+  }
 }
