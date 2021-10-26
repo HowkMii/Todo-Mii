d4ad95c3d727b77282b4a5c57d2eba8463459771
howkmii <abdelhakim@univ-tiaret.dz>
2021-10-26 19:42:52 +0100

    local notification

--- lib/services/notification_services.dart
+++ lib/services/notification_services.dart
@@ -3,8 +3,22 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 
 class NotifyHelper {
-  static const AndroidInitializationSettings InitializationSettingsAndroid =
-      AndroidInitializationSettings('app_icon');
+ FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+// initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+const AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings('appicon');
+final IOSInitializationSettings initializationSettingsIOS =
+    IOSInitializationSettings(
+        onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+final MacOSInitializationSettings initializationSettingsMacOS =
+    MacOSInitializationSettings();
+final InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsIOS,
+    macOS: initializationSettingsMacOS);
+await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+    onSelectNotification: selectNotification);
   Future onDidReceiveLocalNotification(
       int id, String body, String payload) async {
     Get.dialog(Text(body));
