58c04aaaf9dd88ad73ca765b97571ea702599a05
howkmii <abdelhakim@univ-tiaret.dz>
2021-10-27 19:39:20 +0100

    _

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
+import 'package:todomii/services/notification_services.dart';
 import 'package:todomii/services/theme_services.dart';
 import 'package:todomii/ui/theme.dart';
 import 'ui/pages/notification_screen.dart';
 import 'ui/pages/home_page.dart';
-void main() {
+
+void main() async {
   runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized;
+  NotifyHelper().initializeNotification();
 }
 
 class MyApp extends StatelessWidget {
