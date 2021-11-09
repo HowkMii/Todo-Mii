5c998cd670c34bd9474d3d5b66ef4cd52cd50f7d
howkmii <abdelhakim@univ-tiaret.dz>
2021-11-09 22:53:57 +0100

    task tile

--- lib/ui/widgets/task_tile.dart
+++ lib/ui/widgets/task_tile.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+import '../size_config.dart';
+
 class TaskTile extends StatelessWidget {
   const TaskTile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(8)),
+      child: Container(),
+    );
   }
 }
