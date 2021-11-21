f0c8ff294be4441c5573cbac1330b48926c3206d
howkmii <abdelhakim@univ-tiaret.dz>
2021-11-21 21:15:42 +0100

    CREATE TABLE

--- lib/db/db_helper.dart
+++ lib/db/db_helper.dart
@@ -14,13 +14,18 @@ class DBHelper {
     } else {
       try {
         String _path = await getDatabasesPath() + 'task.db';
+        debugPrint('in database path');
         _db = await openDatabase(_path, version: _version,
             onCreate: (Database db, int version) async {
+          debugPrint('creating one');
           await db.execute(
             'CREATE TABLE $_tableName ('
-            'id INTEGER PRIMARY KEY,'
-            'name TEXT, value INTEGER,'
-            'num REAL)',
+            'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+            'title STRING, note TEXT,date STRING,'
+            'startTime STRING, endTime STRING,'
+            'remind INTEGER, repeat STRING,'
+            'color INTEGER,'
+            'isCompleted INTEGER)',
           );
         });
       } catch (e) {
