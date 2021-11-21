19087e5392f3866aca468da12ab5de5bbaa4f2ed
howkmii <abdelhakim@univ-tiaret.dz>
2021-11-21 20:21:54 +0100

    init database

--- lib/db/db_helper.dart
+++ lib/db/db_helper.dart
@@ -4,7 +4,7 @@ import 'package:sqflite/sqflite.dart';
 import '/models/task.dart';
 
 class DBHelper {
-  static Database _db;
+  static Database? _db;
   static final int _version = 1;
   static final String _tableName = 'tasks';
 }
