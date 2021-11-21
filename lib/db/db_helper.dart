bf8874ba94d8622ce1e6e35ddd8ca51e3d8bab44
howkmii <abdelhakim@univ-tiaret.dz>
2021-11-21 20:54:24 +0100

    open database

--- lib/db/db_helper.dart
+++ lib/db/db_helper.dart
@@ -7,4 +7,17 @@ class DBHelper {
   static Database? _db;
   static final int _version = 1;
   static final String _tableName = 'tasks';
+  static Future<void> initDb() async {
+    if (_db != null) {
+      debugPrint('not null db');
+      return;
+    } else {
+      try {
+        String _path = await getDatabasesPath() + 'task.db';
+        _db = await openDatabase('ourdb.db');
+      } catch (e) {
+        print(e);
+      }
+    }
+  }
 }
